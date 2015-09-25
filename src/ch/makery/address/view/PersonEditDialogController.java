0a2f77773a849986bc14077c937379f320c95cd7
xym4er <xym4er@gmail.com>
2015-09-25 10:46:38 +0300

    123

--- src/ch/makery/address/view/PersonEditDialogController.java
+++ src/ch/makery/address/view/PersonEditDialogController.java
@@ -29,28 +29,14 @@ public class PersonEditDialogController {
     private Person person;
     private boolean okClicked = false;
 
-    /**
-     * Initializes the controller class. This method is automatically called
-     * after the fxml file has been loaded.
-     */
     @FXML
     private void initialize() {
     }
 
-    /**
-     * Sets the stage of this dialog.
-     *
-     * @param dialogStage
-     */
     public void setDialogStage(Stage dialogStage) {
         this.dialogStage = dialogStage;
     }
 
-    /**
-     * Sets the person to be edited in the dialog.
-     *
-     * @param person
-     */
     public void setPerson(Person person) {
         this.person = person;
 
@@ -63,18 +49,10 @@ public class PersonEditDialogController {
         birthdayField.setPromptText("dd.mm.yyyy");
     }
 
-    /**
-     * Returns true if the user clicked OK, false otherwise.
-     *
-     * @return
-     */
     public boolean isOkClicked() {
         return okClicked;
     }
 
-    /**
-     * Called when the user clicks ok.
-     */
     @FXML
     private void handleOk() {
         if (isInputValid()) {
@@ -90,19 +68,11 @@ public class PersonEditDialogController {
         }
     }
 
-    /**
-     * Called when the user clicks cancel.
-     */
     @FXML
     private void handleCancel() {
         dialogStage.close();
     }
 
-    /**
-     * Validates the user input in the text fields.
-     *
-     * @return true if the input is valid
-     */
     private boolean isInputValid() {
         String errorMessage = "";
 
